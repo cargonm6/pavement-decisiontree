2d9c6dcf077a10ba122df575fb3ca4d35f901d31
cargonm6 <cargonm6@teleco.upv.es>
2022-10-15 00:45:08 +0200

    Work in progress...!

--- scr/pci_calc.py
+++ scr/pci_calc.py
@@ -1,6 +1,29 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
+dict_distress = {
+    "distress_01": ["ALLIGATOR CRACKING LOW", "ALLIGATOR CRACKING MEDIUM", "ALLIGATOR CRACKING HIGH"],
+    "distress_02": ["BLEEDING LOW", "BLEEDING MEDIUM", "BLEEDING HIGH"],
+    "distress_03": ["BLOCK CRACKING LOW", "BLOCK CRACKING MEDIUM", "BLOCK CRACKING HIGH"],
+    "distress_04": ["BUMPS AND SAGS LOW", "BUMPS AND SAGS MEDIUM", "BUMPS AND SAGS HIGH"],
+    "distress_05": ["CORRUGATION LOW", "CORRUGATION MEDIUM", "CORRUGATION HIGH"],
+    "distress_06": ["DEPRESSION LOW", "DEPRESSION MEDIUM", "DEPRESSION HIGH"],
+    "distress_07": ["EDGE CRACKING LOW", "EDGE CRACKING MEDIUM", "EDGE CRACKING HIGH"],
+    "distress_08": ["JOINT REFLECTION CRACKING LOW", "JOINT REFLECTION CRACKING MEDIUM",
+                    "JOINT REFLECTION CRACKING HIGH"],
+    "distress_09": ["LANE-SHOULDER DROP OFF LOW", "LANE-SHOULDER DROP OFF MEDIUM", "LANE-SHOULDER DROP OFF HIGH"],
+    "distress_10": ["LONG-TRANS CRACKING LOW", "LONG-TRANS CRACKING MEDIUM", "LONG-TRANS CRACKING HIGH"],
+    "distress_11": ["PATCHING LOW", "PATCHING MEDIUM", "PATCHING HIGH"],
+    "distress_12": ["POLISHED AGGREGATE ALL"],
+    "distress_13": ["POTHOLES LOW", "POTHOLES MEDIUM", "POTHOLES HIGH"],
+    "distress_14": ["RAILROAD CROSSING LOW", "RAILROAD CROSSING MEDIUM", "RAILROAD CROSSING HIGH"],
+    "distress_15": ["RUTTING LOW", "RUTTING MEDIUM", "RUTTING HIGH"],
+    "distress_16": ["SHOVING LOW", "SHOVING MEDIUM", "SHOVING HIGH"],
+    "distress_17": ["SLIPPAGE CRACKING LOW", "SLIPPAGE CRACKING MEDIUM", "SLIPPAGE CRACKING HIGH"],
+    "distress_18": ["SWELL LOW", "SWELL MEDIUM", "SWELL HIGH"],
+    "distress_19": ["WEATHERING AND RAVELING LOW", "WEATHERING AND RAVELING MEDIUM", "WEATHERING AND RAVELING HIGH"]
+}
+
 
 # Numeración de los daños
 # 01. Piel de cocodrilo (3 severidades)
@@ -48,7 +71,7 @@ class PCI:
         self.section_id = ""
         self.survey_date = ""
         self.construction_no = 0
-        self.survey_width = 0
+        self.survey_width = 50
         self.section_length = 500.0
         self.section_area = self.survey_width * self.section_length
 
@@ -193,28 +216,116 @@ class PCI:
         if p_distress is None and p_severity is None:
             for x in self.distress:
                 for idx_y, y in enumerate(self.distress[x]):
-                    self.distress[x][idx_y][1] = y[0] / self.section_area
+                    self.distress[x][idx_y][1] = 100 * y[0] / self.section_area
 
         # Si se ha definido un daño en particular,
         # solamente actualiza las densidades de ese daño
         else:
-            self.distress[p_distress][p_severity][1] = self.distress[p_distress][p_severity][0] / self.section_area
+            self.distress[p_distress][p_severity][1] = 100 * self.distress[p_distress][p_severity][
+                0] / self.section_area
+
+    def update_dv(self):
+        """
+        Actualiza los Deduct Value (DV) de cada daño, en función de sus densidades
+        :return:
+        """
+
+        df_curves = pd.read_csv("../csv/curvas_pci.csv", sep=";", encoding="utf-8", decimal=",", low_memory=False)
+
+        for x in self.distress:
+
+            # Si son potholes, se multiplica por 10 para coincidir con las curvas de DV
+            density_factor = 10 if x == "distress_13" else 1
+
+            for idx_y, y in enumerate(self.distress[x]):
+                density = y[1] * density_factor
+
+                # Si la densidad es mayor que cero
+                if density > 0:
+                    # Obtenemos las columnas de la tabla de curvas
+                    col_distress = dict_distress[x][idx_y]
+                    df_curve = df_curves[["DISTRESS", col_distress]]
+
+                    # Busca el primer y último índice válido de cada columna (aquellos no nulos)
+                    curve_min_row = df_curve[[col_distress]].first_valid_index()
+                    curve_max_row = df_curve[[col_distress]].last_valid_index()
+
+                    # Busca el primer y último valor válido de densidad
+                    curve_min_di = df_curve.iloc[curve_min_row]["DISTRESS"]
+                    curve_max_di = df_curve.iloc[curve_max_row]["DISTRESS"]
+
+                    # Busca el primer y último valor válido de DV
+                    curve_min_dv = df_curve.iloc[curve_min_row][col_distress]
+                    curve_max_dv = df_curve.iloc[curve_max_row][col_distress]
+
+                    # Si la densidad excede los límites, o es la menor o la mayor, toma el valor DV extremo
+                    if density < curve_min_di:
+                        deduct_value = curve_min_dv
+                    elif density >= curve_max_di:
+                        deduct_value = curve_max_dv
+                    else:
+                        # Toma el índice del valor de DV igual o inmediatamente inferior al de densidad
+                        idx_density = df_curve[df_curve["DISTRESS"].le(density)].index[-1]
+
+                        # Obtiene el contenido del índice y el siguiente elemento
+                        di_idx0 = df_curve.iloc[idx_density]["DISTRESS"]
+                        di_idx1 = df_curve.iloc[idx_density + 1]["DISTRESS"]
+
+                        dv_idx0 = df_curve.iloc[idx_density][col_distress]
+                        dv_idx1 = df_curve.iloc[idx_density + 1][col_distress]
+
+                        # Cálculo del valor deducido
+                        deduct_value = dv_idx0 + ((dv_idx1 - dv_idx0) / (di_idx1 - di_idx0)) * (density - di_idx0)
+
+                    # Asignación del valor deducido
+                    y[2] = deduct_value
+
+    def get_pci(self) -> float:
+        """
+        Aplica la corrección de DV y obtiene el PCI
+        :return:
+        """
+
+        # Crea una lista de daños con DV en orden descendente
+        df_data = []
+        for x in self.distress:
+            for idx_y, y in enumerate(self.distress[x]):
+                density = y[1]
+                if density > 0:
+                    df_row = {"distress": x, "severity": idx_y, "dv": y[2]}
+                    df_data.append(df_row)
+        df_data = pd.DataFrame(df_data)
+        df_data.sort_values(by=["dv"], ascending=False, inplace=True)
+
+        total = 0
+
+        # Si existen daños
+        if not df_data.empty:
+            # total = df_data["dv"].sum()
+            # Si sólo hay 1 daño o el 2.º daño tiene valor <= 2
+            if len(df_data.index) == 1 or df_data.iloc[1]["dv"] <= 2:
+                total = df_data["dv"].sum()
+            else:
+                m = 1 + 9/98 * (100 - df_data.iloc[0]["dv"])
+                print(m)
+
+                # todo
+                total = df_data["dv"].sum()
+
+        # Si no hay daños, el PCI es 100
+        return 100 - total
 
 
-def import_data(p_data: pd.DataFrame) -> PCI:
+def import_ltpp_data(p_data: pd.DataFrame) -> PCI:
     p_obj = PCI()
 
-    state_code = p_data["STATE_CODE"]
-    section_id = p_data["SHRP_ID"]
-    survey_date = p_data["DATE"]
-    construction_no = p_data["CONSTRUCTION_NO"]
-    # survey_width = p_data["SURVEY_WIDTH"]
-
-    p_obj.set_section(p_state_code=state_code,
-                      p_section_id=section_id,
-                      p_survey_date=survey_date,
-                      p_construction_no=construction_no,
-                      p_survey_width=1)
+    # Datos de la sección
+    p_obj.set_section(p_state_code=p_data["STATE_CODE"],
+                      p_section_id=p_data["SHRP_ID"],
+                      p_survey_date=p_data["DATE"],
+                      p_construction_no=p_data["CONSTRUCTION_NO"],
+                      p_survey_width=p_data["SURVEY_WIDTH"] if 'SURVEY_WIDTH' in p_data and not pd.isna(
+                          p_data["SURVEY_WIDTH"]) else 20)
 
     # 01. Piel de cocodrilo (3 severidades)
     p_obj.set_distress(1, 0, p_data["GATOR_CRACK_A_L"])
@@ -262,18 +373,16 @@ def import_data(p_data: pd.DataFrame) -> PCI:
     # p_obj.set_distress(9, 2, p_data[""])
 
     # 10. Grieta longitudinal/transversal (3 severidades)
-    p_obj.set_distress(10, 0,
-                       p_data["LONG_CRACK_WP_L_L"] + p_data["LONG_CRACK_WP_SEAL_L_L"] + p_data["LONG_CRACK_NWP_L_L"] +
-                       p_data["LONG_CRACK_NWP_SEAL_L_L"] + p_data["TRANS_CRACK_NO_L"] + p_data["TRANS_CRACK_L_L"] +
-                       p_data["TRANS_CRACK_SEAL_L_L"])
-    p_obj.set_distress(10, 1,
-                       p_data["LONG_CRACK_WP_L_M"] + p_data["LONG_CRACK_WP_SEAL_L_M"] + p_data["LONG_CRACK_NWP_L_M"] +
-                       p_data["LONG_CRACK_NWP_SEAL_L_M"] + p_data["TRANS_CRACK_NO_M"] + p_data["TRANS_CRACK_L_M"] +
-                       p_data["TRANS_CRACK_SEAL_L_M"])
-    p_obj.set_distress(10, 2,
-                       p_data["LONG_CRACK_WP_L_H"] + p_data["LONG_CRACK_WP_SEAL_L_H"] + p_data["LONG_CRACK_NWP_L_H"] +
-                       p_data["LONG_CRACK_NWP_SEAL_L_H"] + p_data["TRANS_CRACK_NO_H"] + p_data["TRANS_CRACK_L_H"] +
-                       p_data["TRANS_CRACK_SEAL_L_H"])
+    # p_data["TRANS_CRACK_NO_X"]
+    p_obj.set_distress(10, 0, p_data["LONG_CRACK_WP_L_L"] + p_data["LONG_CRACK_WP_SEAL_L_L"] +
+                       p_data["LONG_CRACK_NWP_L_L"] + p_data["LONG_CRACK_NWP_SEAL_L_L"] +
+                       p_data["TRANS_CRACK_L_L"] + p_data["TRANS_CRACK_SEAL_L_L"])
+    p_obj.set_distress(10, 1, p_data["LONG_CRACK_WP_L_M"] + p_data["LONG_CRACK_WP_SEAL_L_M"] +
+                       p_data["LONG_CRACK_NWP_L_M"] + p_data["LONG_CRACK_NWP_SEAL_L_M"] +
+                       p_data["TRANS_CRACK_L_M"] + p_data["TRANS_CRACK_SEAL_L_M"])
+    p_obj.set_distress(10, 2, p_data["LONG_CRACK_WP_L_H"] + p_data["LONG_CRACK_WP_SEAL_L_H"] +
+                       p_data["LONG_CRACK_NWP_L_H"] + p_data["LONG_CRACK_NWP_SEAL_L_H"] +
+                       p_data["TRANS_CRACK_L_H"] + p_data["TRANS_CRACK_SEAL_L_H"])
 
     # 11. Parcheo (3 severidades)
     p_obj.set_distress(11, 0, p_data["PATCH_A_L"])
@@ -318,12 +427,16 @@ def import_data(p_data: pd.DataFrame) -> PCI:
     p_obj.set_distress(19, 1, p_data["RAVELING"])
     # p_obj.set_distress(19, 2, p_data[""])
 
+    # Actualización de los Deducted Value (DV)
+    p_obj.update_dv()
+    print("pci:", p_obj.get_pci())
+
     return p_obj
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("../csv/pci.csv", sep=";", encoding="unicode_escape", decimal=",", low_memory=False)
+    df = pd.read_csv("../csv/pci.csv", sep=";", encoding="utf-8", decimal=",", low_memory=False)
 
     for df_i in range(0, len(df.index)):
-        pci_obj = import_data(df.iloc[df_i])
-        pci_obj.print_distresses()
+        pci_obj = import_ltpp_data(df.iloc[df_i])
+        # pci_obj.print_distresses()
